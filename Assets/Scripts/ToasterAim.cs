46fb8e5a783f3b5671b5e5324d397ab65eb6247a
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-10-07 15:15:34 -0500

    Floor, rigidbodies, mouselook script
    Added a floor, rigidbodies to toast prefab and floor, and copied the mouselook script from SpaceJam

---
+++ Assets/Scripts/ToasterAim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToasterAim : MonoBehaviour
+{
+    //Make mouse sensitivity editable in the inspector
+    [SerializeField] private float horizontalMouseSensitivity = 0.5f; //Horizontal mouse sensitivity
+    [SerializeField] private float verticalMouseSensitivity = 0.5f; //Vertical mouse sensitivity
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Hide cursor
+    }
+
+    void Update()
+    {
+        float deltaMouseHorizontal = Input.GetAxis("Mouse X") * horizontalMouseSensitivity;
+        float deltaMouseVertical = Input.GetAxis("Mouse Y") * verticalMouseSensitivity;
+        float newCameraRotationY = transform.rotation.eulerAngles.y + deltaMouseHorizontal;
+        float newCameraRotationX = transform.rotation.eulerAngles.x - deltaMouseVertical;
+
+        transform.eulerAngles = new Vector3(newCameraRotationX, newCameraRotationY, 0);
+
+        //Unlock cursor with escape key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None; //Show cursor
+        }
+    }
+}
