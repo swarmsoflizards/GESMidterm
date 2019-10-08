5d063dde98b5c6dc945a7277d15537b60a43af92
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-10-08 12:49:45 -0500

    Limit toast launch per second
    Referenced code from SpaceJam to limit amount of toast launched per second

--- Assets/Scripts/ToastLauncher.cs
+++ Assets/Scripts/ToastLauncher.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class ToastLauncher : MonoBehaviour
 {
-    public GameObject toastPrefab; //Set toast prefab as GameObject
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    [SerializeField] GameObject toastPrefab; //Set toast prefab as GameObject
+    [SerializeField] float launchForce = 5f; //Set launch force
+    [SerializeField] float launchPerSecond = 2; //Limit launch per second
+    bool canLaunch = true; //Set allowed to launch
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (canLaunch && Input.GetKeyDown(KeyCode.Space)) //Launch on spacebar
         {
-            //Launch toast from toaster
             Instantiate(toastPrefab, transform.position, toastPrefab.transform.rotation); //Launch from toaster's position
+            Rigidbody rb = toastPrefab.GetComponent<Rigidbody>(); //Get toast Rigidbody
+
+            Vector3 direction = Quaternion.Euler(15, 0, 0) * transform.forward; //Define launch force
+            rb.AddForce(direction * launchForce, ForceMode.Impulse); //Apply launch force
+            canLaunch = false;
+
+            Invoke("EnableLaunch", 1 / launchPerSecond); //Apply launch per second
         }
+    }
+
+    void EnableLaunch()
+    {
+        canLaunch = true; //Allow launch
     }
 
 }
