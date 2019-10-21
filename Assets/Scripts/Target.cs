a7102f39ed861c81b30268cfb17bdc1fbf8d9241
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-10-21 15:18:11 -0500

    Added target spawn script; NOT COMPLETE

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameObject targetPrefab; //Assign target prefab to be instnatiated
+    public float spawnRangeX; //Set range x
+    public float spawnRangeZ; //Set range z
+    public float startDelay = 2; //Set delay
+    public float spawnInternal = 1.5f; //
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnTarget", startDelay, spawnInternal); //Keep spawning new targets
+    }
+    void SpawnTarget()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+
+        Instantiate(targetPrefab, spawnPos, Quaternion.identity); //Instantiate plate
+    }
+}
