549137752e590d1b064db5419296c23d4d4e98cc
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-10-28 13:49:25 -0500

    Wrote menu script, added button functions, added menu scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject creditsMenu;
+
+    public void StartGame()
+    {
+        mainMenu.SetActive(false);
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+    }
+
+    public void ShowCreditsMenu()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
