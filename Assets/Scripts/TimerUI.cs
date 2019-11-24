d2087a9432bf4dc1f72491961698bc53176fa4f5
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-11-24 15:31:49 -0600

    Deleted dead code

--- Assets/Scripts/TimerUI.cs
+++ Assets/Scripts/TimerUI.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using TMPro; //Need to reference TextMesh Pro
 
 public class TimerUI : MonoBehaviour
 {
     
     [SerializeField] TMP_Text timerLabel; //Assign UI label to time
-    //private float time; //Time variable
     [SerializeField] int timeLeft = 60; //Initial time left
 
     private void Start()
@@ -17,28 +16,16 @@ public class TimerUI : MonoBehaviour
         Time.timeScale = 1; //Make sure that time scale is right
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-
         timerLabel.text = ("" + timeLeft); //Write countdown to canvas
-
-        //time += Time.deltaTime;
-
-        //var minutes = time / 60; //Divide guiTime by 60 to get minutes
-        //var seconds = time % 60; //Euclidean divide to get seconds
-        ////var fraction = (time * 100) % 100;
-
-        //timerLabel.text = string.Format("{0:00} : {1:00}", minutes, seconds); //Update label text
-
     }
 
     IEnumerator Countdown() //Countdown coroutine
     {
         while (true)
         {
-            yield return new WaitForSeconds(1); //
+            yield return new WaitForSeconds(1); //Return every second
             timeLeft--; //Subtract variable
         }
     }
