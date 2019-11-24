667dae08bdb571fb6279fa887c2feccdf202f67b
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-11-24 15:11:57 -0600

    Wrote timer code

--- Assets/Scripts/TimerUI.cs
+++ Assets/Scripts/TimerUI.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    [SerializeField] Text timerLabel; //Assign UI label to time
+    private float time; //Time variable
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        time += Time.deltaTime;
+
+        var minutes = time / 60; //Divide guiTime by 60 to get minutes
+        var seconds = time % 60; //Euclidean divide to get seconds
+        var fraction = (time * 100) % 100;
+
+        timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction); //Update label text
+
     }
 }
