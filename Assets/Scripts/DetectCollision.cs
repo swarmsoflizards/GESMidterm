0f48f63c40b850b5a7d47690496830ded786573c
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-12-01 21:02:18 -0600

    Points being tracked, can't send to TMPro; moved furniture

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -1,30 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; //Need to reference TextMesh Pro
 
 public class DetectCollision : MonoBehaviour
 {
-    //[SerializeField] GameObject toast; //Assign toast
-    //[SerializeField] GameObject target; //Assign plate
+    //[SerializeField] TMP_Text pointsLabel; //Assign UI label to points
+    [SerializeField] int points = 0; //Initial points
     //[SerializeField] float destroyDelay;
 
     [SerializeField] AudioSource soundToastLand; //Play sound
 
-    // Start is called before the first frame update
-    void Start()
+    //public TMP_Text pointsLabel;
+
+    private void Start()
     {
-        
+        ////pointsLabel = gameObject.AddComponent<TextMeshPro>();
+        //TextMeshPro pointsText = GetComponent<TextMeshPro>();
+        //pointsText.text = "Points: " + points;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //pointsLabel.text = ("" + points); //Write points to canvas
     }
 
     private void OnCollisionEnter(Collision collision) //On collision of toast with target
     {
         soundToastLand.Play();
+        points++; //Add points
         Debug.Log("Collision detected");
     }
 
