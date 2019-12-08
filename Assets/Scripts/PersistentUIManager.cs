2c578fcf933c7cac07ffacdc1d222530591863e7
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-12-08 17:05:30 -0600

    Create UI manager singleton

---
+++ Assets/Scripts/PersistentUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentUIManager : MonoBehaviour
+{
+
+    public static PersistentUIManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null) //If there are no other instances of this object
+        {
+            Instance = this; //Set this to the Instance
+            DontDestroyOnLoad(gameObject); //Don't destroy
+        }
+        else //If there ARE other instances of this object
+        {
+            Destroy(gameObject); //Destroy them
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] Canvas UICanvas; //Assign persistent UI Canvas
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
