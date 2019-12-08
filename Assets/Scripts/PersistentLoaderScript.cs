a9426cb133908986667f6a794fa408f42f323267
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-12-08 01:12:27 -0600

    Created SceneLoader singleton

---
+++ Assets/Scripts/PersistentLoaderScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentLoaderScript : MonoBehaviour
+{
+    public static PersistentLoaderScript Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
