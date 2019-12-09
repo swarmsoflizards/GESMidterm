e133fe65e375e0fdbe731e532fa56f05ac8bb3e7
Dan Steinmetz <emma.steinmetz@gmail.com>
2019-12-08 20:37:21 -0600

    Added end screen to timer script, panel activates and text prints

--- Assets/Scripts/TimerUI.cs
+++ Assets/Scripts/TimerUI.cs
@@ -10,8 +10,12 @@ public class TimerUI : MonoBehaviour
     [SerializeField] TMP_Text timerLabel; //Assign UI label to time
     [SerializeField] int timeLeft = 60; //Initial time left
 
+    [SerializeField] GameObject endMenu; //Define end screen
+    [SerializeField] TMP_Text endLabel; //Define end text
+
     private void Start()
     {
+        endMenu.SetActive(false); //Hide end menu
         StartCoroutine("Countdown"); //Start countdown coroutine
         Time.timeScale = 1; //Make sure that time scale is right
     }
@@ -26,7 +30,15 @@ public class TimerUI : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(1); //Return every second
-            timeLeft--; //Subtract variable
+            if (timeLeft > 0) //If there is time left
+            {
+                timeLeft--; //Subtract variable
+            }
+            else if (timeLeft <= 0) //If time runs out
+            {
+                endMenu.SetActive(true); //Show end screen
+                endLabel.text = ("You've run out of time!");
+            }
         }
     }
 }
